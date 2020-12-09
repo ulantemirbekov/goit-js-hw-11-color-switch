20d1defd5e03be67f8c89635652b313b698730a3
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-12-09 22:01:44 +0200

    solution added

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const bodyRef = document.querySelector('body');
+const startRef = document.querySelector('[data-action="start"]');
+const finishRef = document.querySelector('[data-action="stop"]');
+
+let timerId;
+
+function colorSwitcher() {
+    timerId = setInterval(() => {
+        bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+        startRef.disabled = true;
+        finishRef.disabled = false;
+    }, 1000);
+};
+
+startRef.addEventListener('click', colorSwitcher);
+finishRef.addEventListener('click', () => {
+    clearInterval(timerId);
+    startRef.disabled = false;
+    finishRef.disabled = true;
+});
+
+
